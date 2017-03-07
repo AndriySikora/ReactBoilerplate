94f8c6d869dc91bf69b48cc3c0ed783d6dc98595
AndriySikora <andriy.sikora@gmail.com>
2017-03-07 15:33:00 +0200

    update axios

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -19,35 +19,3 @@ module.exports = {
         });
     }
 }
-
-// module.exports = {
-// getTemp: function(location) {
-//   const encodedLocation = encodeURIComponent(location);
-//   const requestUrl = `${openWeatherMapUrl}&q=${encodedLocation}`;
-//
-//   var myHeaders = new Headers();
-//
-//   var myInit = {  method: 'GET',
-//                   headers: myHeaders,
-//                   crossDomain: true,
-//                   mode: 'cors',
-//                   cache: 'default' };
-//
-//   var myRequest = new Request(requestUrl, myInit);
-//
-//   return fetch(myRequest)
-//   .then(function(response) {
-//       if(response.ok) {
-//           return response.data.main.temp;
-//       }
-//       throw new Error('Network response was not ok.');
-//     })
-//     .then(function(myBlob) {
-//         var objectURL = URL.createObjectURL(myBlob);
-//     })
-//     .catch(function(error) {
-//         console.log('There has been a problem with your fetch operation: ' + error.message);
-//     });
-// }
-//
-// }
